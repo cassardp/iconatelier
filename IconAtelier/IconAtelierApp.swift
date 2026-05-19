c9356a19103164e0ad3f0743396be5bd18101421
Patrice Cassard <cassard2@gmail.com>
2026-05-19 07:47:07 +0200

    feat(app): force light color scheme app-wide
    Lock the UI to light mode regardless of the system setting so previews and
    color editing stay predictable.

--- IconAtelier/IconAtelierApp.swift
+++ IconAtelier/IconAtelierApp.swift
@@ -3,12 +3,15 @@ import SwiftUI
 @main
 struct IconAtelierApp: App {
     @State private var store = ProjectStore()
+    @State private var presetStore = PresetStore()
 
     var body: some Scene {
         WindowGroup {
             GalleryView()
                 .fontDesign(.rounded)
+                .preferredColorScheme(.light)
                 .environment(store)
+                .environment(presetStore)
         }
     }
 }
